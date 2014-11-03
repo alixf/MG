9023c8b576bbf4005a8243e37ab0b2124a2ba4c5
Vic-King <victorfrancois@free.fr>
2014-11-03 16:10:17 +0100

    Correction recalibration

--- src/recalibration.cpp
+++ src/recalibration.cpp
@@ -1,6 +1,6 @@
 #include <Eigen/Geometry>
 
-#include "recalibration.hpp"
+#include "recalibration.h"
 
 void Recalibration::translate (std::vector<float>& vertices, const QVector3D &p) {
 
@@ -11,16 +11,23 @@ void Recalibration::translate (std::vector<float>& vertices, const QVector3D &p)
     }
 }
 
-void Recalibration::rotate (GLfloat* vertices, const QVector3D &angle) {
+void Recalibration::rotate (std::vector<float>& vertices, const QVector3D &angle) {
 
-    Eigen::AngleAxisf angleX (angle.x, QVector3D(1, 0, 0));
-    Eigen::AngleAxisf angleY (angle.y, QVector3D(0, 1, 0));
-    Eigen::AngleAxisf angleZ (angle.z, QVector3D(0, 0, 1));
+    Eigen::Vector4f vec;
+    Eigen::Vector4f vecRot;
 
-    Eigen::Quaternionf q = Quaternionf(angle);
+    Eigen::Quaternionf q = Eigen::Quaternionf(0, angle.x(), angle.y(), angle.z());
+    Eigen::Quaternionf qInv = q.inverse();
 
     for (int i = 0; i < vertices.size(); i += 3) {
-        // TODO
+
+        vec = Eigen::Vector4f(vertices[i], vertices[i+1], vertices[i+2], 0);
+
+        vecRot = q * vec * qInv;
+
+        vertices[i] = vecRot.x();
+        vertices[i+1] = vecRot.y();
+        vertices[i+2] = vecRot.z();
     }
 }
 
@@ -31,19 +38,58 @@ void Recalibration::recalibrate (std::vector<float>& vertices, const QVector3D &
     translate(vertices, vecTranslation);
 
     // Rotation
-    float angle = angleBetweenVectors(v1p, v2, v1p, v2); // Attention v1 doit valoir v1p (ou l'inverse -> vérifier)
-    rotate (vertices, angle);
+    QVector3D angle1 = angleBetweenVectors(v1p, v2, v1p, v2); // A vérifier
+    QVector3D angle2 = angleBetweenVectors(v1p, v3, v1p, v3); // A vérifier
 
-    // Modifier modèle
+    rotate (vertices, angle1 + angle2);
 }
 
 QVector3D Recalibration::vectorPointToPoint(const QVector3D &p1,const  QVector3D &p2) const{
 
-    return QVector3D(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
+    return QVector3D(p2.x() - p1.x(), p2.y() - p1.y(), p2.z() - p1.z());
 }
 
 
-float Recalibration::angleBetweenVectors(const QVector3D& ov1, const QVector3D& fv1, const QVector3D& ov2, const QVector3D& fv2) const {
+QVector3D Recalibration::angleBetweenVectors(const QVector3D& ov1, const QVector3D& fv1, const QVector3D& ov2, const QVector3D& fv2) const {
+
+    QVector3D res;
+
+    // Axe X
+
+    QVector3D dirv1 = QVector3D(fv1.x() - fv1.x(), 0, 0);
+    QVector3D dirv2 = QVector3D(fv2.x() - fv2.x(), 0, 0);
+
+    float scalar = dirv1.x() * dirv2.x();
+
+    float norm = sqrt(dirv1.x() * dirv1.x()) * sqrt(dirv2.x() * dirv2.x());
+
+    res.setX(acos(scalar / norm));
+
+    // Axe Y
+
+    dirv1 = QVector3D(0, fv1.y() - fv1.y(), 0);
+    dirv2 = QVector3D(0, fv2.y() - fv2.y(), 0);
+
+    scalar = dirv1.y() * dirv2.y();
+
+    norm = sqrt(dirv1.y() * dirv1.y()) * sqrt(dirv2.y() * dirv2.y());
+
+    res.setY(acos(scalar / norm));
+
+    // Axe Z
+
+    dirv1 = QVector3D(fv1.z() - fv1.z(), 0, 0);
+    dirv2 = QVector3D(fv2.z() - fv2.z(), 0, 0);
+
+    scalar = dirv1.z() * dirv2.z();
+
+    norm = sqrt(dirv1.z() * dirv1.z()) * sqrt(dirv2.z() * dirv2.z());
+
+    res.setZ(acos(scalar / norm));
+
+    return res;
+
+    /*
 
     QVector3D dirv1 = QVector3D(fv1.x - fv1.x, fv1.y - ov1.y, fv1.z - ov1.z);
     QVector3D dirv2 = QVector3D(fv2.x - fv2.x, fv2.y - ov2.y, fv2.z - ov2.z);
@@ -53,5 +99,5 @@ float Recalibration::angleBetweenVectors(const QVector3D& ov1, const QVector3D& 
     float norm = sqrt(dirv1.x() * dirv1.x() + dirv1.y() * dirv1.y() + dirv1.z() * dirv1.z()) *
                  sqrt(dirv2.x() * dirv2.x() + dirv2.y() * dirv2.y() + dirv2.z() * dirv2.z());
 
-    return acos(scalarP / norm);
+    return acos(scalarP / norm);*/
 }
--- src/recalibration.h
+++ src/recalibration.h
@@ -1,15 +1,20 @@
 #ifndef RECALIBRATION_H
 #define RECALIBRATION_H
 
+#include <QVector3D>
+#include <QPoint>
+
 class Recalibration
 {
 public :
 
 
 private :
-    void Recalibration::translate (std::vector<float>& vertices, const QVector3D &p);
-    void rotate (GLfloat* vertices, const QVector3D &angle);
-    QVector3F vectorPointToPoint(const QVector3D &p1,const  QVector3D &p2) const;
+    void translate (std::vector<float>& vertices, const QVector3D &p);
+    void rotate (std::vector<float>& vertices, const QVector3D &angle);
+    void recalibrate (std::vector<float>& vertices, const QVector3D &v1, const QVector3D &v2, const QVector3D &v3, const QVector3D &v1p, const QVector3D &v2p, const QVector3D &v3p);
+    QVector3D vectorPointToPoint(const QVector3D &p1,const  QVector3D &p2) const;
+    QVector3D angleBetweenVectors(const QVector3D& ov1, const QVector3D& fv1, const QVector3D& ov2, const QVector3D& fv2) const;
 };
 
 #endif // RECALIBRATION_H
